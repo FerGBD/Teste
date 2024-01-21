8e14d809bbd8014ceebba0b4e660d9478b98267c
FerGBD <fergabriel1409@gmail.com>
2024-01-20 23:31:22 -0300

    inicio javaScript

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Welcome!");
+let numeroSecreto = 29;
+let chute = prompt("Escolha um número entre 1 e 30");
+
+if (numeroSecreto == chute){
+    console.log("Right number!")
+}else{
+    console.log("Wrong number")
+}
